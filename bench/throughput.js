e3ad20b936fa49dfcea3779bb2d321f1e5f48e48
Maciej Małecki <me@mmalecki.com>
2014-02-10 14:53:52 +0100

    Use `bench` module

--- bench/throughput.js
+++ bench/throughput.js
@@ -2,72 +2,51 @@ var net = require('net');
 var crypto = require('crypto');
 var zmq = require('zmq');
 var ZMTP = require('../');
+var port = Math.floor(Math.random() * 4096 + 1024);
 
-var on = true;
-var mgs = 0;
-
-var RANDOM = crypto.randomBytes(128);
-
-function reallyRun(zmtp) {
-  zmtp.send(RANDOM);
-  on && setImmediate(function () {
-    reallyRun(zmtp);
-  });
-}
-
-function run(zmtp, cb) {
-  msg = 0;
-  on = true;
-  setTimeout(function () {
-    on = false;
-    cb(msg);
-  }, 10000);
-
-  reallyRun(zmtp);
-}
-
-function zmtp(cb) {
-  console.log('zmtp');
-  var server = net.createServer(function (sock) {
-    var zmtp = new ZMTP({ type: 'pull' });
-    sock.pipe(zmtp).pipe(sock);
-    zmtp.on('message', function () {
-      ++msg;
-    });
-  }).listen(0, function () {
-    var zmtp = new ZMTP({ type: 'push' });
-    var conn = net.connect(server.address().port);
-    conn.pipe(zmtp).pipe(conn);
-
-    zmtp.on('ready', function () {
-      run(zmtp, function (msg) {
-        console.log('ztmp: ' + msg + ' messages in 10 s');
-        conn.end();
-        server.close();
-        cb();
-      });
-    });
-  });;
-}
-
-function zmq_(cb) {
-  console.log('zmq');
-  var server = zmq.socket('pull');
-  server.bindSync('tcp://127.0.0.1:9000');
-  server.on('message', function () {
-    ++msg;
-  });
-
-  var client = zmq.socket('push');
-  client.connect('tcp://127.0.0.1:9000');
-  run(client, function (msg) {
-    console.log('zmq: ' + msg + ' messages in 10 s');
-    client.close();
-    server.close();
+function runner(zmtpish, buffer) {
+  return function (cb) {
+    zmtpish.send(buffer);
     cb();
-  });
+  };
 }
 
-zmq_(function () {
-  zmtp(function () {});
-});
+function runZmq(size) {
+  var buffer = crypto.randomBytes(size);
+  var server = zmq.socket('pull');
+  var client;
+  server.bindSync('tcp://127.0.0.1:' + port);
+
+  client = zmq.socket('push');
+  client.connect('tcp://127.0.0.1:' + port++);
+
+  return runner(client, buffer);
+}
+
+function runZmtp(size) {
+  var buffer = crypto.randomBytes(size);
+  var push = new ZMTP({ type: 'push' });
+  var server = net.createServer(function (sock) {
+    var pull = new ZMTP({ type: 'pull' });
+    sock.pipe(pull).pipe(sock);
+  }).listen(0, function () {
+    var conn = net.connect(server.address().port);
+    conn.pipe(push).pipe(conn);
+  });
+  return runner(push, buffer);
+}
+
+exports.compare = {
+  'zmq, size=128': runZmq(128),
+  'zmtp, size=128': runZmtp(128),
+  'zmq, size=256': runZmq(256 - 1),
+  'zmtp, size=256': runZmtp(256 - 1),
+  'zmq, size=512': runZmq(512 - 1),
+  'zmtp, size=512': runZmtp(512 - 1),
+  'zmq, size=1024': runZmq(1024 - 2),
+  'zmtp, size=1024': runZmtp(1024 - 2)
+};
+
+setTimeout(function () {
+  require('bench').runMain();
+}, 1000);
